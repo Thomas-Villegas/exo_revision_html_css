aa559c6576c0cf08d4ee370062f01b5b649f286d
Thomas-Villegas <116794064+Thomas-Villegas@users.noreply.github.com>
2023-06-20 15:56:16 +0200

    FINISH

---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+// Premièrement, obtenons tous les éléments dont nous avons besoin.
+let dots = Array.from(document.querySelectorAll('.dot'));
+let expressions = Array.from(document.querySelectorAll('.section2_box_expression > div:not(.section2_quote)'));
+
+// Maintenant, ajoutons un gestionnaire d'événement à chaque "dot".
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    // Ajoute la classe 'off' à toutes les expressions.
+    expressions.forEach(exp => exp.classList.add('off'));
+    // Supprime la classe 'off' de l'expression correspondante à ce "dot".
+    expressions[index].classList.remove('off');
+  });
+});
